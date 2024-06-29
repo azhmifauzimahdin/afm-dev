0b858ad1282852e93c73cebcdaafce9c678bb35a
azhmifauzimahdin <azhmifauzi11@gmail.com>
2024-06-29 23:40:27 +0700

    Filtering projects

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,7 +1,6 @@
 import { Link } from "@inertiajs/react";
 
-export default function Pagination({links}) {
-    console.log(links);
+export default function Pagination({ links }) {
     return (
         <nav className="text-center mt-4">
             {links.map((link) => (
@@ -13,9 +12,9 @@ export default function Pagination({links}) {
                         "inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
                         (link.active ? "bg-gray-950 " : " ") +
                         (!link.url ? "!text-gray-500 cursor-not-allowed " : "hover:bg-gray-950")
-                      }
+                    }
                     dangerouslySetInnerHTML={{ __html: link.label }}
-                    >
+                >
                 </Link>
             ))}
         </nav>
